2d00e2288ae65c6c88bd267dbafae867ed76cd66
thepm-cpu <thepm002@gmail.com>
2025-10-19 20:08:25 +0100

    fixed bucket issue

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,8 +12,3 @@ output "ec2_instance_id" {
   description = "ID of the EC2 instance"
   value       = aws_instance.app.id
 }
-
-output "ansible_s3_bucket" {
-  description = "S3 bucket for Ansible SSM"
-  value       = aws_s3_bucket.ansible_ssm.bucket
-}
